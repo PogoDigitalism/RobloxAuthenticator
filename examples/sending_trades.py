06cd3a5f3d2d07cc22581ea859470638ef453d96
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-10-30 16:56:31 +0100

    Update sending_trades.py

--- examples/sending_trades.py
+++ examples/sending_trades.py
@@ -1,1 +1,17 @@
-h
+from utils import Formatting
+from tradeauthenticator import TradeAuthenticator, TradeAuthenticatorAsync
+
+t = TradeAuthenticator()
+t.add(USER_ID = 98452948, 
+      OTP_SECRET = 'OTPSECRETHERE', 
+      RBLX_COOKIE = 'ROBLOSECURITYCOOKIEHERE', 
+      TAG = 'acc1')
+
+data = utils.Formatting.TradeData(SENDER_USER_ID = 98452948, 
+                                  RECIPIENT_USER_ID = 89389334, 
+                                  OFFER = [19873344479], 
+                                  REQUEST = [59884936856], 
+                                  ROBUX = 35, 
+                                  RECIPIENT_ROBUX = 230)
+
+t.send_trade('acc1', data)
