8ea0cf502ce12cb8ebfe92d147e78e0b2fc722ee
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-10-30 16:57:51 +0100

    Create accepting_trades.py

---
+++ examples/accepting_trades.py
@@ -0,0 +1,9 @@
+from tradeauthenticator import TradeAuthenticator, TradeAuthenticatorAsync
+
+t = TradeAuthenticator()
+t.add(USER_ID = 98452948, 
+      OTP_SECRET = 'OTPSECRETHERE', 
+      RBLX_COOKIE = 'ROBLOSECURITYCOOKIEHERE', 
+      TAG = 'acc1')
+
+t.accept_trade('acc1', TRADE_ID = YOURIDHERE)
