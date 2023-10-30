c4b0e664b96253cee4423ae16a30666df7c21990
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-10-30 18:53:22 +0100

    Update sending_trades.py

--- examples/sending_trades.py
+++ examples/sending_trades.py
@@ -12,6 +12,6 @@ data = Formatting.TradeData(SENDER_USER_ID = 98452948,
                                   OFFER = [19873344479], 
                                   REQUEST = [59884936856], 
                                   ROBUX = 35, 
-                                  RECIPIENT_ROBUX = 230)
+                                  RECIPIENT_ROBUX = 230) #Formatting.TradeData is not required as it returns just a dictionairy that you could build yourselves. For the convenience however, a utility was added either way to easily format them.
 
 t.send_trade('acc1', data)
