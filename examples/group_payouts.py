e83bb91aebcd7cee0af32fd38da9cfb74f148b87
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-10-31 16:41:24 +0100

    Update group_payouts.py

--- examples/group_payouts.py
+++ examples/group_payouts.py
@@ -8,7 +8,7 @@ t.add(USER_ID = 98452948,
       RBLX_COOKIE = 'ROBLOSECURITYCOOKIEHERE', 
       TAG = 'acc1')
 
-data = Formatting.OneTimePayout(RECIPIENT_USER_ID = 98452948, ROBUX = 10)
+data = Formatting.OneTimePayout(PAYOUT_RECIPIENT_USER_ID = 98452948, ROBUX = 10)
 
 try:
     t.one_time_payout('acc1', GROUP_ID = 10435498, PAYOUT_DATA = data)
