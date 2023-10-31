d0c942fed0fbd447040dc42fee2de32185982989
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-10-31 11:22:29 +0100

    Update sending_trades.py

--- examples/sending_trades.py
+++ examples/sending_trades.py
@@ -1,11 +1,12 @@
 from utils import Formatting
-from authenticator import TradeAuthenticator, TradeAuthenticatorAsync
+from authenticator import Authenticator
+from exceptions import *
 
-t = TradeAuthenticator()
+t = Authenticator()
 t.add(USER_ID = 98452948, 
       OTP_SECRET = 'OTPSECRETHERE', 
       RBLX_COOKIE = 'ROBLOSECURITYCOOKIEHERE', 
-      TAG = 'acc1') # adds a Roblox account to the account cache containing the information required to make requests.
+      TAG = 'acc1')
 
 data = Formatting.TradeData(SENDER_USER_ID = 98452948, 
                                   RECIPIENT_USER_ID = 89389334, 
@@ -14,4 +15,8 @@ data = Formatting.TradeData(SENDER_USER_ID = 98452948,
                                   ROBUX = 35, 
                                   RECIPIENT_ROBUX = 230) #Formatting.TradeData is not required as it returns just a dictionairy that you could build yourselves. For the convenience however, a utility was added either way to easily format them.
 
-t.send_trade(TAG = 'acc1', TRADE_DATA = data)
+try:
+      t.send_trade(TAG = 'acc1', TRADE_DATA = data)
+except AlreadyProcessedError:
+    # Do whatever u want here if it processed without authentication
+    pass
