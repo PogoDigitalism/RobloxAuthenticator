baf76086bde068bbfc419e1010082ef6f229eb49
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-10-31 16:42:09 +0100

    Update sending_trades.py

--- examples/sending_trades.py
+++ examples/sending_trades.py
@@ -9,7 +9,7 @@ t.add(USER_ID = 98452948,
       TAG = 'acc1')
 
 data = Formatting.TradeData(SENDER_USER_ID = 98452948, 
-                                  RECIPIENT_USER_ID = 89389334, 
+                                  TRADE_RECIPIENT_USER_ID = 89389334, 
                                   OFFER = [19873344479], 
                                   REQUEST = [59884936856], 
                                   ROBUX = 35, 
