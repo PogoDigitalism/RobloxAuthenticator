2c65cc3b67561dd1c1d714019e8644b79cdb09ff
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-10-31 12:03:57 +0100

    Update exceptions.py

--- src/exceptions.py
+++ src/exceptions.py
@@ -3,5 +3,15 @@ class APIError(Exception):
         super().__init__(*args)
 
 class AlreadyProcessedError(Exception):
+    """
+    Is raised when a request did not require authentication to complete.
+    
+    It is highly recommended that you wrap your functions in a 
+    
+    try: 
+        *
+    except AlreadyProcessedError:
+    
+    """
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
