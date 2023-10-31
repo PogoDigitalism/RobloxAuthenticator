45b2e13f554865f72b741dc5e3401767104796eb
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-10-31 18:21:28 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -54,7 +54,7 @@ class Config:
             'COOKIES': ['.ROBLOSECURITY'],
             'HEADERS': ['x-csrf-token', 'Content-Type'],
             'DATA': 'GROUP',
-            'STATUS': [400],
+            'STATUS': [403],
             'RETURN_HEADERS': ["rblx-challenge-metadata", "rblx-challenge-id", "rblx-challenge-type"],
             'PROCESSING': [['_getMetaDataChallengeId'],['challengeId']]
         },
