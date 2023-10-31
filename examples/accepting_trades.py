f02e0b52ed70dfb77347eccda96d06c471d0c596
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-10-31 11:21:40 +0100

    Update accepting_trades.py

--- examples/accepting_trades.py
+++ examples/accepting_trades.py
@@ -1,9 +1,15 @@
-from authenticator import TradeAuthenticator, TradeAuthenticatorAsync
+from utils import Formatting
+from authenticator import Authenticator
+from exceptions import *
 
-t = TradeAuthenticator()
+t = Authenticator()
 t.add(USER_ID = 98452948, 
       OTP_SECRET = 'OTPSECRETHERE', 
       RBLX_COOKIE = 'ROBLOSECURITYCOOKIEHERE', 
       TAG = 'acc1')
 
-t.accept_trade('acc1', TRADE_ID = YOURIDHERE)
+try:
+    t.accept_trade('acc1', TRADE_ID = YOURIDHERE)
+except AlreadyProcessedError:
+    # Do whatever u want here if it processed without authentication
+    pass
