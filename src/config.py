d715095a5ff722b839ddf1eede76d78864304dda
Pogo <107322523+PogoDigitalism@users.noreply.github.com>
2023-11-21 12:30:32 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,12 +6,12 @@ class Config:
         'CHALLENGEID':
             {
                 'SEND': 'https://trades.roblox.com/v1/trades/send',
-                'ACCEPT': 'https://trades.roblox.com/v1/trades/$TRADE_ID$/accept',
+                'ACCEPT': 'https://trades.roblox.com/v1/trades/$TRADE_ID$/accept'
             },
         'GROUP_CHALLENGEID':
             {
                 'GROUP_ONE_TIME_PAYOUT': 'https://groups.roblox.com/v1/groups/$GROUP_ID$/payouts',
-                'GROUP_RECURRING_PAYOUT': 'https://groups.roblox.com/v1/groups/$GROUP_ID$/payouts/recurring',
+                'GROUP_RECURRING_PAYOUT': 'https://groups.roblox.com/v1/groups/$GROUP_ID$/payouts/recurring'
             },
         'TRADE':
             {
@@ -21,7 +21,7 @@ class Config:
         'GROUP':
             {
                 'GROUP_ONE_TIME_PAYOUT': 'https://groups.roblox.com/v1/groups/$GROUP_ID$/payouts',
-                'GROUP_RECURRING_PAYOUT': 'https://groups.roblox.com/v1/groups/$GROUP_ID$/payouts/recurring',
+                'GROUP_RECURRING_PAYOUT': 'https://groups.roblox.com/v1/groups/$GROUP_ID$/payouts/recurring'
             }
     }
     
@@ -54,7 +54,17 @@ class Config:
             'COOKIES': ['.ROBLOSECURITY'],
             'HEADERS': ['x-csrf-token', 'Content-Type'],
             'DATA': 'GROUP',
-            'STATUS': [403],
+            'STATUS': [400],
+            'RETURN_HEADERS': ["rblx-challenge-metadata", "rblx-challenge-id", "rblx-challenge-type"],
+            'PROCESSING': [['_getMetaDataChallengeId'],['challengeId']]
+        },
+        'PURCHASE_CHALLENGEID': {
+            'METHOD': 'POST',
+            'URL': 'https://economy.roblox.com/v1/purchases/products/$ACCESSORY_ID$',
+            'COOKIES': ['.ROBLOSECURITY'],
+            'HEADERS': ['x-csrf-token', 'Content-Type'],
+            'DATA': 'PURCHASE',
+            'STATUS': [400],
             'RETURN_HEADERS': ["rblx-challenge-metadata", "rblx-challenge-id", "rblx-challenge-type"],
             'PROCESSING': [['_getMetaDataChallengeId'],['challengeId']]
         },
@@ -104,13 +114,26 @@ class Config:
             'RETURN_HEADERS' : [],
             'PROCESSING': None
         },
+        'PURCHASE': {
+            'METHOD': 'POST',
+            'URL': 'https://economy.roblox.com/v1/purchases/products/$ACCESSORY_ID$',
+            'COOKIES': ['.ROBLOSECURITY'],
+            'HEADERS': ['x-csrf-token', 'Content-Type'],
+            'DATA': 'PURCHASE',
+            'STATUS': [200],
+            'RETURN_HEADERS' : [],
+            'PROCESSING': None
+        },
     }
+    
+    # https://economy.roblox.com/v1/purchases/products/1327881684
     
     SEQUENCECONFIG = {
         'SEND': ['XCSRF', 'CHALLENGEID', 'TWOSTEP', 'CONTINUE', 'TRADE'],
         'ACCEPT': ['XCSRF', 'CHALLENGEID', 'TWOSTEP', 'CONTINUE', 'TRADE'],
         'GROUP_ONE_TIME_PAYOUT': ['XCSRF', 'GROUP_CHALLENGEID', 'TWOSTEP', 'CONTINUE', 'GROUP'],
-        'GROUP_RECURRING_PAYOUT': ['XCSRF', 'GROUP_CHALLENGEID', 'TWOSTEP', 'CONTINUE', 'GROUP'],       
+        'GROUP_RECURRING_PAYOUT': ['XCSRF', 'GROUP_CHALLENGEID', 'TWOSTEP', 'CONTINUE', 'GROUP'],
+        'ACCESSORY_PURCHASE': ['XCSRF', 'PURCHASE_CHALLENGEID', 'TWOSTEP', 'CONTINUE', 'PURCHASE']    
     }
 
     METHOD_ARGS =  {
@@ -120,9 +143,11 @@ class Config:
         'accept_trade': ['TAG', 'TRADE_ID'],
         'send_trade': ['TAG', 'TRADE_DATA'],
         'one_time_payout': ['TAG', 'GROUP_ID', 'PAYOUT_DATA'],
+        'accessory_purchase': ['TAG', 'ACCESSORY_ID', 'PURCHASE_DATA'],
         'TradeData': ['SENDER_USER_ID', 'TRADE_RECIPIENT_USER_ID', 'OFFER', 'REQUEST', 'ROBUX', 'RECIPIENT_ROBUX'],
         'OneTimePayout': ['PAYOUT_RECIPIENT_USER_ID', 'ROBUX'],
         'RecurringPayout': ['PAYOUT_RECIPIENT_USER_ID', 'PERCENTAGE'],
+        'AccessoryPurchase': ['PRICE', 'SELLER_ID'],
     }
     
     VALIDATE_TYPES = {'USER_ID': Union[str, int],
@@ -137,7 +162,11 @@ class Config:
                   'PAYOUT_RECIPIENT_USER_ID': int,
                   'GROUP_ID': int,
                   'PAYOUT_DATA': dict,
+                  'PURCHASE_DATA': dict,
                   'PERCENTAGE': int,
+                  'ACCESSORY_ID': int,
+                  'SELLER_ID': int,
+                  'PRICE': int,
                   
                   'TRADE_RECIPIENT_USER_ID': Union[str, int],
                   'SENDER_USER_ID': int,
